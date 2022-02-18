d758ec69d9ad3d8482133ba76e11396f51a6433c
tenstand <christensmith_@msn.com>
2022-02-18 11:49:56 +0000

    add ifs and else ifs to see who has won the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,26 @@ function playGame(playerChoice){
 
     let computerChoice = Math.floor(Math.random() * 5);
 
+    if (computerChoice === 1) {
+        computerChoice = 'rock';
+    }
+
+    if (computerChoice === 2) {
+        computerChoice = 'paper';
+    }
+
+    if(computerChoice === 3) {
+        computerChoice = 'scissors';
+    }
+
+    if(computerChoice === 4) {
+        computerChoice = 'lizard';
+    }
+
+    if(computerChoice === 5) {
+        computerChoice = 'spock';
+    }
+
     computerImage.src = 'assets/images${choices[computerChoice]}.jpg';
     computerImage.alt = choices[computerChoice];
 
@@ -25,3 +45,24 @@ function playGame(playerChoice){
 
     updateScores(result);
 }
+
+// See who's won
+
+ if (playerChoice === computerChoice)
+       result = ("It's a tie!"); 
+    else if (playerChoice === "Rock"){
+      if (computerChoice ==="Scissors"){
+        result("You win!!");
+    } else if (computerChoice === "Paper"){ 
+        result("Computer wins! Try again!"); 
+    } else if (playerChoice ==="Paper"){ 
+       if (computerChoice ==="Scissors")
+       result("Computer wins! Try again!"); 
+    } else if (computerChoice === "Rock"){ 
+        result("You win!"); 
+    }else if (playerChoice === "Scissors"){
+        if (computerChoice === "Paper") 
+        result =("You win!"); 
+    ] else if (computerChoice ==="Rock"){ 
+        result("Computer wins! Try again!"); 
+    
