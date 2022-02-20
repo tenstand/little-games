11b752ea4ae0d45b3f79d3858030acd2f1212675
tenstand <christensmith_@msn.com>
2022-02-20 16:18:22 +0000

    move main-image to top

--- assets/js/jscript2.js
+++ assets/js/jscript2.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var computerScore = 0;
     var userScore = 0;
 
@@ -8,18 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', userPlay);
     }
 
-    function userPlay(event){
+    function userPlay(event) {
         const clickedButton = event.target;
         const userChoice = clickedButton.getAttribute('data-choice');
         const userImage = document.getElementById('userPlay');
         userImage.setAttribute('src', './assets/images/' + userChoice + '.jpg');
         const computerChoice = computerPlay();
-        console.log('Computer played ' + computerChoice + ' user played '+ userChoice);
+        console.log('Computer played ' + computerChoice + ' user played ' + userChoice);
         roundWinner(userChoice, computerChoice);
-         
+
     }
 
-    function computerPlay(){
+    function computerPlay() {
         const computerChoice = Math.floor(Math.random() * 5);
         const plays = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
         const computerImage = document.getElementById('computerPlay');
@@ -27,82 +27,68 @@ document.addEventListener('DOMContentLoaded', function() {
         return plays[computerChoice];
     }
 
-    function computerWins(){
+    function computerWins() {
         computerScore = computerScore + 1;
-        alert('Computer wins Score '+ computerScore);
+        alert('Computer wins Score ' + computerScore);
     }
 
-    function userWins(){
+    function userWins() {
         userScore = userScore + 1;
-        alert('User wins Score '+ userScore);
+        alert('User wins Score ' + userScore);
         document.getElementById('player-score').innerText = userScore;
     }
 
-    function roundWinner(userChoice, computerChoice){
-        if (userChoice === computerChoice){
+    function roundWinner(userChoice, computerChoice) {
+        if (userChoice === computerChoice) {
             alert(' its a tie');
-        }
-        else{
+        } else {
             // User Rock
-            if (userChoice === 'rock'){
+            if (userChoice === 'rock') {
                 const beatsRock = ['paper', 'spock']
-                if (beatsRock.includes(computerChoice)){
+                if (beatsRock.includes(computerChoice)) {
                     computerWins();
-                }
-                else{
+                } else {
                     userWins();
                 }
             }
-        else{  
             // User Paper
-            if (userChoice === 'paper'){
+            else if (userChoice === 'paper') {
                 const beatsPaper = ['scissors', 'lizard']
-                if (beatsPaper.includes(computerChoice)){
+                if (beatsPaper.includes(computerChoice)) {
                     computerWins();
-                }
-                else{
+                } else {
                     userWins();
-                }    
-        else{  
-             // User Scissors
-            if (userChoice === 'scissors'){
-            const beatsScissors = ['spock', 'rock']
-            if (beatsPaper.includes(computerChoice)){
-                computerWins();
                 }
-                else{
+            }
+            // User Scissors
+            else if (userChoice === 'scissors') {
+                const beatsScissors = ['spock', 'rock']
+                if (beatsPaper.includes(computerChoice)) {
+                    computerWins();
+                } else {
                     userWins();
-                }    
-        else{  
+                }
+            }
             // User Lizard
-            if (userChoice === 'lizard'){
-            const beatsLizard = ['scissors', 'rock']
-            if (beatsPaper.includes(computerChoice)){
-                computerWins();
-                }
-                else{
+            else if (userChoice === 'lizard') {
+                const beatsLizard = ['scissors', 'rock']
+                if (beatsPaper.includes(computerChoice)) {
+                    computerWins();
+                } else {
                     userWins();
-                }            
-        else{  
+                }
+            }
             // User Spock
-            if (userChoice === 'spock'){
-            const beatsLizard = ['lizard', 'paper']
-            if (beatsPaper.includes(computerChoice)){
-                computerWins();
-                }
-                else{
+            else if (userChoice === 'spock') {
+                const beatsSpock = ['lizard', 'paper']
+                if (beatsPaper.includes(computerChoice)) {
+                    computerWins();
+                } else {
                     userWins();
-                }            
+                }
+            }
         }
-        
+
     }
 
 });
- 
-
-
-
-
-
-
-
